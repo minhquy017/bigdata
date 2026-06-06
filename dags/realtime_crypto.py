1842bc14ebcdb038cb1b5eaa2b42214da0260feb
MinhQuy <minhquy017@gmail.com>
2026-06-06 16:47:36 +0700

    quylhm update pipeline

--- dags/realtime_crypto.py
+++ dags/realtime_crypto.py
@@ -7,13 +7,20 @@ import io
 import boto3
 from botocore.client import Config
 
-
 def crawl_realtime_job():
     print("Bắt đầu lấy dữ liệu phút mới nhất...")
     exchange = ccxt.bitstamp()
 
     btc_ohlcv = exchange.fetch_ohlcv('BTC/USD', '1m', limit=1000)
     df_btc = pd.DataFrame(btc_ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+    
+    # --- 1. BỘ LỌC RÁC ---
+    # Ép các cột giá về chuẩn số, loại bỏ dòng chứa rác (như chữ "60s")
+    numeric_cols = ['open', 'high', 'low', 'close', 'volume']
+    for col in numeric_cols:
+        df_btc[col] = pd.to_numeric(df_btc[col], errors='coerce')
+    df_btc = df_btc.dropna(subset=numeric_cols)
+    
     df_btc['timestamp'] = pd.to_datetime(df_btc['timestamp'], unit='ms')
     df_btc['timestamp'] = df_btc['timestamp'] + pd.Timedelta(hours=7)
     
@@ -25,12 +32,20 @@ def crawl_realtime_job():
         config=Config(signature_version='s3v4')
     )
     
+    # --- 2. ĐOẠN CODE TẠO BUCKET BẠN VIẾT TRÊN JUPYTER ---
+    BUCKET_NAME = "crypto-raw-data"
+    try:
+        s3_client.head_bucket(Bucket=BUCKET_NAME)
+        print(f"Bucket '{BUCKET_NAME}' đã tồn tại.")
+    except:
+        s3_client.create_bucket(Bucket=BUCKET_NAME)
+        print(f"Đã tự động tạo mới Bucket: '{BUCKET_NAME}' trên MinIO.")
 
+    # Ghi file
     csv_buffer = io.StringIO()
     df_btc.to_csv(csv_buffer, index=False)
-    s3_client.put_object(Bucket="crypto-raw-data", Key='bitcoin_1m.csv', Body=csv_buffer.getvalue())
-    print(f"Hoàn thành cập nhật {len(df_btc)} dòng dữ liệu realtime!")
-
+    s3_client.put_object(Bucket=BUCKET_NAME, Key='bitcoin_1m.csv', Body=csv_buffer.getvalue())
+    print(f"Hoàn thành cập nhật {len(df_btc)} dòng dữ liệu realtime sạch!")
 
 default_args = {
     'owner': 'minhquy',
